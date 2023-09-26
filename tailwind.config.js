90081b8eb5172f7de1abd9ef1aa9bbf58a271281
mhusendev <husenrgc.96@gmail.com>
2023-09-27 01:47:32 +0700

    update v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/**/*.{html,js}","./src/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-   
-    extend: {
-      width: {
-        '98':'30rem'
-      },
-      keyframes:{
-        bounceme:{
-          '0%': {
-            transform: 'translateY(0)'
-          },
-       
-          '20%': {
-            transform: 'translateY(-15%)'
-          },
-        
-            '40%': {
-              transform: 'translateY(0)'
-            },
-            '60%': {
-              transform: 'translateY(-15%)'
-            },
-            '80%': {
-              transform: 'translateY(0)'
-            },
-            '100%': {
-              transform: 'translateY(0)'
-            },
-        
-       
-          
-        }
-      },
-      animation:{
-        'bounce-me': 'bounceme 20s infinite'
-      }
-    },
+    extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'),
-    require("daisyui"),
-    
-  ],
-
+  plugins: [],
 }
